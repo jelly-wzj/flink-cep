10c065160ba22a16f28636755cfca054fec41874
源码中的汉语拼音 <haode888@163.com>
2021-02-15 21:51:06 +0800

    [#00017] update code editor ui

--- roc-web/roc-web-client/src/components/ConsoleEditor.vue
+++ roc-web/roc-web-client/src/components/ConsoleEditor.vue
@@ -7,25 +7,62 @@
                     placeholder="请选择模板引擎"
                     size="small"
                     style="width:150px"
-                    @change="onEditorModeChange"
-            >
+                    @change="onEditorModeChange">
                 <el-option
                         v-for="item in cmEditorModeOptions"
                         :key="item"
                         :label="item"
-                        :value="item"
-                ></el-option>
+                        :value="item"></el-option>
             </el-select>
         </el-header>
-        <el-main class="console_main">
-            <code-mirror-editor
-                    ref="cmEditor"
-                    :cmTheme="cmTheme"
-                    :cmMode="cmMode"
-                    :autoFormatJson="autoFormatJson"
-                    :jsonIndentation="jsonIndentation"
-            ></code-mirror-editor>
-        </el-main>
+        <el-container>
+            <el-main class="console_main">
+                <code-mirror-editor
+                        ref="cmEditor"
+                        :cmTheme="cmTheme"
+                        :cmMode="cmMode"
+                        :autoFormatJson="autoFormatJson"
+                        :jsonIndentation="jsonIndentation"></code-mirror-editor>
+            </el-main>
+            <el-aside class="console_side">
+                    <el-main class="source_main">
+                        <span>输入源:  </span>
+                        <el-form>
+                            <el-select v-model="cmEditorMode"
+                                       placeholder="Type"
+                                       size="small"
+                                       @change="onEditorModeChange" style="width: 250px">
+                                <el-option v-for="item in storageTypes"
+                                           :key="item"
+                                           :label="item"
+                                           :value="item"/>
+                            </el-select>
+                            <el-input placeholder="Host" size="small" style="width: 250px"/>
+                            <el-input placeholder="Fields" size="small" style="width: 250px"/>
+                            <el-input placeholder="Storage" size="small" style="width: 250px"/>
+                            <el-input placeholder="Auth" size="small" style="width: 250px"/>
+                        </el-form>
+                    </el-main>
+                    <el-main class="source_main">
+                        <span>输出源: </span>
+                        <el-form>
+                            <el-select v-model="cmEditorMode"
+                                       placeholder="Type"
+                                       size="small"
+                                       @change="onEditorModeChange" style="width: 250px">
+                                <el-option v-for="item in storageTypes"
+                                           :key="item"
+                                           :label="item"
+                                           :value="item"/>
+                            </el-select>
+                            <el-input placeholder="Host" size="small" style="width: 250px"/>
+                            <el-input placeholder="Fields" size="small" style="width: 250px"/>
+                            <el-input placeholder="Storage" size="small" style="width: 250px"/>
+                            <el-input placeholder="Auth" size="small" style="width: 250px"/>
+                        </el-form>
+                    </el-main>
+            </el-aside>
+        </el-container>
         <el-footer>
             <el-button type="primary" size="medium" style="margin-top: 10px" @click="setValue">重置</el-button>
             <el-button type="primary" size="medium" style="margin-left: 10px" @click="getValue">提交作业</el-button>
@@ -55,7 +92,14 @@
                 // json编辑模式下，json格式化缩进 支持字符或数字，最大不超过10，默认缩进2个空格
                 jsonIndentation: 2,
                 // json编辑模式下，输入框失去焦点时是否自动格式化，true 开启， false 关闭
-                autoFormatJson: true
+                autoFormatJson: true,
+                storageTypes: [
+                    "kafka",
+                    "hbase",
+                    "mysql",
+                    "elasticsearch",
+                    "redis"
+                ]
             };
         },
         methods: {
@@ -118,5 +162,18 @@
         margin-top: 10px;
         padding-top: 10px;
         height: 450px;
+        width: 50%;
+    }
+
+    .console_side {
+        padding-left: 5px;
+        background-color: #ececec;
+        margin-top: 10px;
+        padding-top: 10px;
+    }
+
+    .source_main{
+        padding-right: 200px;
+        background-color: #ececec;
     }
 </style>
